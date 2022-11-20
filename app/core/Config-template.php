57a1b85616dbc76ba9ac77a188f1b0572c0244a1
TennNiedziedz <krawczyk_wojciech@wp.pl>
2022-11-20 17:02:02 +0100

    Config.php temaplate added

---
+++ app/core/Config-template.php
@@ -0,0 +1,7 @@
+<?php
+    //Change name for Config.php
+    //Database params
+    define('DB_HOST', 'localhost'); //Add your db host
+    define('DB_USER', ''); // Add your DB root
+    define('DB_PASS', ''); //Add your DB pass
+    define('DB_NAME', ''); //Add your DB Name
